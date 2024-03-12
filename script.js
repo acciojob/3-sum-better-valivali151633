7bf3a518a9a354993c755c2b23f2cfbffecb6417
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-03-12 19:12:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function threeSum(arr, target) {
-// write your code here
-  
-}
+    // write your code here
+        let closestSum = Infinity;
+        // let sum ; 
+        
+        for(let i=0 ; i<arr.length-2 ; i++)
+            {
+                for(let j=i+1 ; j<arr.length-1; j++)
+                    {
+                        for(let k=j+1 ; k<arr.length ; k++)
+                            {
+                                let sum = arr[i] + arr[j] +arr[k];
+                                console.log(arr[i] , arr[j] ,arr[k])
+                                let diff = Math.abs(target-sum)
+                                console.log("diff" , diff)
+                                
+                                if(diff < closestSum)
+                                {
+                                    closestSum = diff;
+                                }
+                            }
+                    }
+            }
+        return closestSum;
+      
+    }
 
 module.exports = threeSum;
