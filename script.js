abd72065c4496f3e515e9a8a0bc74e781aed3c89
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2024-03-13 17:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
-function threeSum(arr, target) {
-    // write your code here
-        let closestSum = Infinity;
-        // let sum ; 
+// function threeSum(arr, target) {
+//     // write your code here
+//         let closestSum = Infinity;
+//         // let sum ; 
         
-        for(let i=0 ; i<arr.length-2 ; i++)
-            {
-                for(let j=i+1 ; j<arr.length-1; j++)
-                    {
-                        for(let k=j+1 ; k<arr.length ; k++)
-                            {
-                                let sum = arr[i] + arr[j] +arr[k];
-                                // console.log(arr[i] , arr[j] ,arr[k])
-                                let diff = Math.abs(target-sum)
-                                // console.log("diff" , diff)
+//         for(let i=0 ; i<arr.length-2 ; i++)
+//             {
+//                 for(let j=i+1 ; j<arr.length-1; j++)
+//                     {
+//                         for(let k=j+1 ; k<arr.length ; k++)
+//                             {
+//                                 let sum = arr[i] + arr[j] +arr[k];
+//                                 // console.log(arr[i] , arr[j] ,arr[k])
+//                                 let diff = Math.abs(target-sum)
+//                                 // console.log("diff" , diff)
                                 
-                                if(diff < closestSum)
-                                {
-                                    closestSum = diff;
-                                }
-                            }
-                    }
-            }
-        return closestSum;
+//                                 if(diff < closestSum)
+//                                 {
+//                                     closestSum = diff;
+//                                 }
+//                             }
+//                     }
+//             }
+//         return closestSum;
       
+//     }
+
+
+function threeSum(nums, target) {
+    nums.sort((a, b) => a - b); // Sort the array
+
+    let closestSum = nums[0] + nums[1] + nums[2]; // Initialize closest sum
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                closestSum = sum; // Update closest sum if needed
+            }
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return closestSum; // Return if exact match found
+            }
+        }
     }
+    return closestSum; // Return closest sum
+}
 
 module.exports = threeSum;
